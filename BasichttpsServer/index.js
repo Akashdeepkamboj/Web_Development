f81c0d4a82054f33cde240fa27ba6fd6f7417819
Akashdeepkamboj <akashdeepkamboj47@gmail.com>
2020-09-13 00:27:24 +0530

    Update index.js
    add comments for the code

--- BasichttpsServer/index.js
+++ BasichttpsServer/index.js
@@ -1,11 +1,11 @@
-const https = require('http');
+const https = require('http'); 
 const port = 8080;
 
 
-const server = https.createServer();
+const server = https.createServer(); // server created
 
 
-server.listen(port,err =>{
+server.listen(port,err =>{           //server start listening on port
     if(err){
         console.log('server not able to start error is coming')
     }
